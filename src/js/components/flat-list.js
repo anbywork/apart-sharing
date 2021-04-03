e6d59b70597e80195dcb43c17834e4c12a3db2da
Anastasia <rusinova.anastasia@gmail.com>
2021-04-03 20:08:27 +1000

    adaptive for iphonex

--- src/js/components/flat-list.js
+++ src/js/components/flat-list.js
@@ -1,7 +1,8 @@
 export const setFlatListPosition = () => {
   const flatList = document.querySelector('.flats__list');
   const flatListWidth = flatList.scrollWidth;
-  const flat = flatList.querySelector('.flat');
-  const flatWidth = flat.offsetWidth;
-  flatList.scrollLeft = flatListWidth / 2 - flatWidth / 2 - 25;
+
+  const screenWidth = document.documentElement.offsetWidth;
+
+  flatList.scrollLeft = flatListWidth / 2 - screenWidth / 2;
 };
