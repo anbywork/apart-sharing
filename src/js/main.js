2b1a639d6960d405968c8b871f644fd4a3780176
Anastasia <rusinova.anastasia@gmail.com>
2021-04-23 19:28:35 +1000

    правки от 22 апреля

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import {setAccordions} from "./components/accordion";
 import {anchorAnimationScroll} from "./components/anchor-animation-scroll";
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
+import {showFAQ} from "./views/faq";
 
 const FLAT_COUNT = 3;
 function render(parentElement, place, template,) {
@@ -24,33 +25,35 @@ const client = new ApolloClient({
 client.query({
   query: gql`
           query {
-            apartments {
+            faq(code: "customer") {
               data {
-                title
-                apartments_images {
-                  image_png
-                }
-                
-                rating
-                reviews_amount
-                city
-                street
-                building
+                answer
+                question
               }
-          
-              status
             }
+            apartments {
+                        data {
+                          title
+                          apartments_images {
+                            image_png
+                          }
+                          
+                          rating
+                          reviews_amount
+                          city
+                          street
+                          building
+                        }
+                    
+                        status
+                      }
           }
       `,
 })
   .then((data) => {
-    const dataFlats = data.data.apartments.data;
-    const flatsList = document.querySelector('.flats__list');
-    for (let i = 0; i < Math.min(FLAT_COUNT, dataFlats.length); i++) {
-      render (flatsList, 'beforeend', createFlatTemplate(dataFlats[i], i));
-    }
-    setFlatListPosition();
-
+    showFlats(data.data.apartments.data);
+    showFAQ(data.data.faq.data);
+    setAccordions();
     const sliders = document.querySelectorAll('.slider');
     for(let slider of Array.from(sliders)) {
       setSlider(slider);
@@ -59,12 +62,19 @@ client.query({
   .catch(error => console.error(error));
 
 
+function showFlats(dataFlats) {
+  const flatsList = document.querySelector('.flats__list');
+  for (let i = 0; i < Math.min(FLAT_COUNT, dataFlats.length); i++) {
+    render (flatsList, 'beforeend', createFlatTemplate(dataFlats[i], i));
+  }
+  setFlatListPosition();
+}
 
 
 setBtnsList();
 setBurger();
 setPageHeader();
-setAccordions();
+
 anchorAnimationScroll();
 
 
---
+++ src/js/views/faq.js
@@ -0,0 +1,18 @@
+function setItem (question, answer) {
+  return `<li class="accordion__item">
+          <button class="accordion__toggle">${question}</button>
+          <div class="accordion__description">
+            ${answer}
+          </div>
+        </li>`
+}
+
+
+export const showFAQ = (data) => {
+  const listFAQ = document.querySelector('.faq__list');
+  for (let faqItem of data) {
+    const item = document.createElement('div');
+    item.innerHTML = setItem(faqItem.question, faqItem.answer);
+    listFAQ.appendChild(item.querySelector('li'));
+  }
+};
